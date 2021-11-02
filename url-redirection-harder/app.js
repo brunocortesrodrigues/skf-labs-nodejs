c1287af1c43439194f3c24ed3cadd52eeab96838
Bruno Rodrigues <brunocortesrodrigues85@gmail.com>
2021-11-02 11:24:55 +0100

    url-redirection-harder

---
+++ url-redirection-harder/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("", (req, res) => {
+  res.render("index.ejs", { content: "Sorry, this is the old website." });
+});
+
+app.get("/newsite", (req, res) => {
+  res.render("index.ejs", { content: "Welcome to the new website!" });
+});
+
+app.post("/redirect", (req, res) => {
+  let newurl = req.query.newurl;
+  if (newurl.includes(".")) {
+    res.render("index.ejs", {
+      content: 'Sorry, you cannot use "." in the redirect',
+    });
+  } else res.redirect(302, newurl);
+});
+
+app.use(function (req, res) {
+  res.status(404).render("404.ejs");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}...!!!`));
