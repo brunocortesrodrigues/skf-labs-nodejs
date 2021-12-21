50deafd69c655a05df5701add38678681cdb8b61
Bruno Rodrigues <brunocortesrodrigues85@gmail.com>
2021-12-21 10:53:57 +0100

    auth-bypass

---
+++ Auth-Bypass/evil_server.js
@@ -0,0 +1,36 @@
+const cookieSession = require("cookie-session");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["e5ac-4ebf-03e5-9e29-a3f562e10b22"],
+    httpOnly: false,
+    maxAge: 86400000,
+  })
+);
+
+app.use(express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("", (req, res) => {
+  req.session.userId = 2;
+  req.session.secret = "e5ac-4ebf-03e5-9e29-a3f562e10b22";
+  req.session.loggedIn = true;
+  console.log(req.session);
+  res.render("evil.ejs");
+});
+
+app.use(function (req, res) {
+  res.status(404).render("404.ejs");
+});
+
+const port = process.env.PORT || 1337;
+
+app.listen(port, () => console.log(`Listening on port ${port}...!!!`));
--- Prototype-pollution/app.js
+++ Prototype-pollution/app.js
@@ -1,16 +1,60 @@
 const express = require("express");
+const session = require("express-session");
+const _ = require("lodash");
 const app = express();
 
 app.use(express.static(__dirname + "/static"));
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+const users = [
+  { name: "user", password: "123456" },
+  { name: "admin", password: "admin", canPost: true },
+];
+
+let messages = [];
+let id = 1;
+
+const findUser = (user, pwd) => {
+  return users.find((u) => u.name === user && u.password === pwd);
+};
+
 app.get("", (req, res) => {
-  res.render("index.ejs", { xss: null });
+  res.render("index.ejs");
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const user = findUser(username, password);
+  if (user) {
+    req.session.user = user;
+    res.render("home.ejs", { xss: null });
+  }
 });
 
 app.post("/home", (req, res) => {
-  let userInput = req.body.string;
-  res.render("index.ejs", { xss: userInput });
+  if (req.session.user.canPost) {
+    let userInput = req.body.string;
+    const message = { title: "test" };
+    _.merge(message, userInput, {
+      id: id++,
+      user: req.session.user,
+    });
+    console.log(req.session.user);
+    messages.push(message);
+    console.log(messages);
+    res.render("home.ejs", { xss: userInput });
+  } else {
+    res.render("home.ejs", { xss: null });
+  }
 });
 
 app.use(function (req, res) {
