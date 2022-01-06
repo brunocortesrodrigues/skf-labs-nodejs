b441d696c76496346284eed7da6eda9f13ad484e
Bruno Rodrigues <brunocortesrodrigues85@gmail.com>
2022-01-06 14:33:39 +0100

    url

--- Url-redirection/app.js
+++ Url-redirection/app.js
@@ -12,7 +12,7 @@ app.get("/newsite", (req, res) => {
   res.render("index.ejs", { content: "Welcome to the new website!" });
 });
 
-app.post("/redirect", (req, res) => {
+app.all("/redirect", (req, res) => {
   let newurl = req.query.newurl;
   res.redirect(302, newurl);
 });
