82a8716b83551491a27b9e916f6f281a4582d6de
Bruno Rodrigues <brunocortesrodrigues85@gmail.com>
2022-01-17 15:08:45 +0100

    Ldap-Injection

--- JWT-null/app.js
+++ JWT-null/app.js
@@ -76,6 +76,3 @@ app.use(function (req, res) {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}...!!!`));
-
-/* const token = req.headers.authorization.split(" ")[1];
-  console.log(token); */
